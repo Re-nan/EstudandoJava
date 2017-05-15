ec6fe7a1bdb31399206e03efb8462ee29f8809ef
renan <renan.ps@outlook.com>
2017-05-15 11:23:36 -0300

    Adicionando comentário sobre superclasse

--- EstudandoJava/src/estudando/heranca/Carro.java
+++ EstudandoJava/src/estudando/heranca/Carro.java
@@ -17,6 +17,11 @@ public class Carro extends Veiculo{
         System.out.println(getMarca() + " " + getModelo()+ " buzinando");
     }
 
+    /* Não existe herança de construtores, o que existe é que cada classe tem seu contrutor
+     * e na hora de construir o obj de uma classe você tem que ir invocando construtores de
+     * superclasse até você chamar o construtor de toda a hierarquia das classes que compoem
+     * aquela estrutura que você está tentando instanciar 
+     */
     @Override
     public void imprimirDados() {
         super.imprimirDados();

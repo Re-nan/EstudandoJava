99c70a7d8c3e6fbdc8aa284e719085b0487aae65
renan <renan.ps@outlook.com>
2017-05-19 00:04:55 -0300

    Correção de comentários no estudo sobre Polimorfismo 1

--- EstudandoJava/src/estudando/polimorfismo1/Main.java
+++ EstudandoJava/src/estudando/polimorfismo1/Main.java
@@ -1,12 +1,12 @@
 package estudando.polimorfismo1;
 
 /**
- * Variavel = é o que ta na esquerda Referência = é o que ta na direita
+ * Referência = é o que ta na esquerda  Instancia = é o que ta na direita
  *
- * No polimofismo o tipo da variavel que fica na esquerda é quem define os
- * métodos que eu posso enxergar vindo da referência(new AlgumaCoisa) da
- * direita, o tipo da varariável só enxerga o que ela tem em comum com a
- * referencia
+ * No polimofismo o tipo da variavel(que é uma referencia) que fica na esquerda
+ * é quem define os métodos que eu posso enxergar vindo da instancia(new AlgumaCoisa)
+ * da direita, o tipo da varariável(tipo da referência) só enxerga o que ela tem em
+ * comum com a instancia
  * 
  * Quem define o método que vai ser chamado é sempre quem ta no new AlgumaCoisa
  * Ou seja sempre quem ta na memória do heap
@@ -17,8 +17,7 @@ public class Main {
 		Cachorro c = new Cachorro();
 		Animal a = new Animal();
 
-		/**
-		 * por mais que faça o cast, continua chamando o método da referencia c
+		/* por mais que faça o cast, continua chamando o método da instancia c
 		 * de Cachorro.
 		 */
 		a.falar(); // Animal falando...
@@ -29,14 +28,12 @@ public class Main {
 		c = (Cachorro) a;
 		c.morder();
 
-		/**
-		 * Enquanto seu obj for Animal você só pode enxergar o que for comum
+		/* Enquanto seu obj for Animal você só pode enxergar o que for comum
 		 * entre Animal e a classe que herdou de Animal
 		 */
 		System.out.println("");
 
-		/**
-		 * Estou perguntando a2 é uma variavel que aponta na memoria para um obj
+		/* Estou perguntando a2 é uma variavel que aponta na memoria para um obj
 		 * do tipo cachorro? Se sim, eu posso converter um tipo a para o tipo 
 		 * cachorro, então nesse caso é seguro fazer o cast
 		 */
@@ -48,8 +45,7 @@ public class Main {
 
 		System.out.println("");
 
-		/**
-		 * Qual a vantagens de eu referenciar um determinado obj por um tipo de
+		/* Qual a vantagens de eu referenciar um determinado obj por um tipo de
 		 * uma superclasse dele se eu não tenho acesso aos metodos especificos?
 		 * Ex. Animal a = new Cachorro(); Animal é a superclasse de Cachorro A
 		 * vantagem é que eu consigo trabalhar com tipos mais genéricos o que
@@ -64,13 +60,12 @@ public class Main {
 
 	}
 
-	/**
-	 * Com um método mais generécico eu posso reutilizar ele com vários objetos
+	/* Com um método mais generécico eu posso reutilizar ele com vários objetos
 	 * de tipos diferentes. Hoje é um Gato e Cachorro, amanhã podem ter outros
 	 * animais, e a chamada já estará pronta. Com polimorfismo você consegue com
 	 * que durante a execução os mátodos que vc chama se comportem de forma
-	 * diferente de acordo com o obj que está anteciados na memória. Quando você
-	 * us apolimorfismo você consegue preparar sua aplicação com coisas que irão
+	 * diferente de acordo com o obj que está anteciado na memória. Quando você
+	 * usa polimorfismo você consegue preparar sua aplicação com coisas que irão
 	 * acontecer depois
 	 */
 	private static void falar(Animal a) {

f04b4e86671f5dd50ac6fa6799c998eb983e313e
renan <renan.ps@outlook.com>
2017-05-21 03:16:10 -0300

    Adicionando comentários sobre Conatantes

--- EstudandoJava/src/estudando/Constantes/Main.java
+++ EstudandoJava/src/estudando/Constantes/Main.java
@@ -1,5 +1,18 @@
 package estudando.Constantes;
 
+/* Usando atribudos staticos no JAVA você pode criar constantes. Uma constante 
+ * normalmente é PÚBLICA para que ela possa ser acessada fora da classe, ela é
+ * FINAL pra evitar que o valor seja alterado, e por ser um elemento stático
+ * você chama ela direto na classe
+ * 
+ * Você é obrigado inicializar ela com um valor, esse valor pode ser definido
+ * na hora de criação, ou deixar que um bloco static o atribua
+ * 
+ * static{
+ *  MEDIA_DA_PROVA = 7.0;
+ * }
+ */
+
 public class Main {
     public static void main(String[] args){
         

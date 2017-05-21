5d8f084214d0708b31ed3a5cb76b409139890a48
renan <renan.ps@outlook.com>
2017-05-21 03:15:06 -0300

    Adicionando estudos sobre Constantes

---
+++ EstudandoJava/src/estudando/Constantes/Constantes.java
@@ -0,0 +1,7 @@
+package estudando.Constantes;
+
+public class Constantes {
+    
+    public static final double MEDIA_FINAL = 7.0;
+    
+}
---
+++ EstudandoJava/src/estudando/Constantes/Main.java
@@ -0,0 +1,10 @@
+package estudando.Constantes;
+
+public class Main {
+    public static void main(String[] args){
+        
+        //Constantes.MEDIA_DA_PROVA = 8.0; Erro, vc não pode atribuir um valor p uma constante
+        System.out.println("Média da prova: " + Constantes.MEDIA_FINAL);
+    }
+    
+}

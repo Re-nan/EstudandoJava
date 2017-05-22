f9af5b5105f73cbb71515fc17d24fe6edfeccde5
renan <renan.ps@outlook.com>
2017-05-22 17:13:27 -0300

    Adicionndo comentários sobre Construtores

--- EstudandoJava/src/estudando/Construtores/Retangulo.java
+++ EstudandoJava/src/estudando/Construtores/Retangulo.java
@@ -5,6 +5,26 @@ public class Retangulo {
     double largura;
     double altura;
 
+    /* Construtor padrão = instancia os atributos da classe com valores padrão
+     * Construtor com parametros = força a instanciação da classe com valores
+     * 
+     * Ele poder ter uma lógica como se fosse um método, pode validar parametros
+     * inclusive pode chamar outros métodos da própria classe, instanciar outros
+     * objetos, ele pode de tudo, a diferença dele pra um método é que ele é
+     * chamado na hora em que o objeto é criado, e o método é chamado 
+     * esplicitamente pelo usuário
+     */
+    
+    /* O this chama o outro construtor da mesma classe, o this tem que ser a 
+     * primeira coisa a ser chamada em um construtor que implementa outro
+     * 
+     * Se chama o construtor padrão, fica sendo como se tivesse chamado o outro
+     * também.
+     * 
+     * Não confunda o this() do construtor com o this. de atribuição de atributos
+     * são coisas totalmente diferentes. O this() invoca o outro construtor da
+     * mesma classe
+     */
     public Retangulo() {
         this(4, 3);
 

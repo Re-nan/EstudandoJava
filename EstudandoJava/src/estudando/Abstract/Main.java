0d4fc8445169be7518f695998b5c149114919c56
renan <renan.ps@outlook.com>
2017-05-25 00:54:09 -0300

    Adicionando comentários no estudo sobre Abstract

--- EstudandoJava/src/estudando/Abstract/Main.java
+++ EstudandoJava/src/estudando/Abstract/Main.java
@@ -1,5 +1,31 @@
 package estudando.Abstract;
 
+/* Classes abstratas são utilizadas quando não faz sentido termos instancias
+ * de determinadas classes, se por algum motivo vc quer barrar a criação de
+ * instancias de uma classe que você escreveu? ela deve ser uma classe abstrata
+ * isso permite manter a consistência do código.
+ * Ex.
+ * Não compila 
+ * Animal a = new Animal() pois Animal é uma classe abstrata
+ * Porém, compila
+ * animal a = new Cachorro() pois a instancia no caso não é de Animal, mas sim
+ * de Cachorro, Animal é apenas o tipo da variavel que aponta pro obj na memória
+ * do tipo Cachorro
+ */ 
+ 
+/* Método abstratos são utilizados quando não faz sentido termos a implementação
+ * do método em determinada classe. 
+ * Não são implementados public abstract void pegar();
+ */
+
+/* Classes abstratas não precisam obrigatoriamente ter métodos abstratos, a classe
+ * pode conter métodos abstratos(os sem implementação) como também métodos normais
+ * (com implmentação)
+ * PORÉM... métodos abstratos SÓ PODEM existir em classes abstratas, pois não tem
+ * como eu ter um método abstract(sem implementação) em uma classe que pode ser
+ * instanciada
+ */
+
 public class Main {
     public static void main(String[] args){
         

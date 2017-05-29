1d6768d40510586e574fe48829c1cd0c0ec9856f
renan <renan.ps@outlook.com>
2017-05-29 20:13:27 -0300

    Adicionando comentários no estudo sobre Stack Trace 2

--- EstudandoJava/src/estudando/exception/StackTrace2/Main.java
+++ EstudandoJava/src/estudando/exception/StackTrace2/Main.java
@@ -11,7 +11,24 @@ public class Main {
 		} catch (AcimaVelocidadeMaximaException e) {
 
 			System.out.println("Erro: Acima da velocidade Permitida");
-			e.printStackTrace(); 								
+			e.printStackTrace(); // Caso queira, mesmo tratando o erro, você pode
+									// pedir para exibir a Stack Trace
 		}
 	}
+
+	// Erro: Acima da velocidade Permitida
+	// estudando.exception.StackTrace.AcimaVelocidadeMaximaException: Excedeu a
+	// velocidade máxima
+	// at estudando.exception.StackTrace.Automovel.acelerar(Automovel.java:11)
+	// at estudando.exception.StackTrace.Main.main(Main.java:42)
+
+	/* Mais uma vez a aplicação começou na classe Main com o método main, ai
+	 * dentro do método main na linha 10 foi chamado o método acelerar
+	 * 
+	 * Dentro da classe Automovel na linha 11 ao atender a clausula do if
+	 * foi lançado uma exceção para AcimaVelocidadeMaximaException
+	 * 
+	 * De volta a classe Main o método tratou a exceção com try catch e o
+	 * "e.printStackTrace();" monstrou o processo de execução na Stack Trace
+	 */ 
 }

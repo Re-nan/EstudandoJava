028daee38fa4186863d1388639b5897e88b82085
renan <renan.ps@outlook.com>
2017-05-28 23:54:36 -0300

    Adicionando estudo sobre Stack Trace 1

---
+++ EstudandoJava/src/estudando/exception/StackTrace1/AcimaVelocidadeMaximaException.java
@@ -0,0 +1,9 @@
+package estudando.exception.StackTrace1;
+
+@SuppressWarnings("serial")
+public class AcimaVelocidadeMaximaException extends Exception {
+
+	public AcimaVelocidadeMaximaException(String message) {
+		super(message);
+	}
+}
---
+++ EstudandoJava/src/estudando/exception/StackTrace1/Main.java
@@ -0,0 +1,12 @@
+package estudando.exception.StackTrace1;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		Automovel a = new Automovel();
+
+		a.acelerar(100);
+		a.acelerar(90);
+
+	}
+}

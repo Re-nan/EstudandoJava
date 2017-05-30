2cc7b1bf10b07134ec28317b59afa4548ee4f818
renan <renan.ps@outlook.com>
2017-05-30 00:59:16 -0300

    Adicionando comentário no estudo sobre Unchecked Exception mais comuns

--- EstudandoJava/src/estudando/exception/UncheckedExceptionMaisComuns/Agenda.java
+++ EstudandoJava/src/estudando/exception/UncheckedExceptionMaisComuns/Agenda.java
@@ -1,5 +1,6 @@
 package estudando.exception.UncheckedExceptionMaisComuns;
 
+//Está classe serve para agrupar os contatos que você cria
 public class Agenda {
 
     private Contato contato1;
@@ -31,9 +32,11 @@ public class Agenda {
     }
 
     public void imprimirNomes(Agenda a) {
-    	
+        //imprime pq já foi atribuido no main
         System.out.println(a.getContato1().getNome());
 
+        //se contato 2 for diferente de null, ele ignora, com isso vc protege
+        //seu condigo de NullpointerException
         if (a.getContato2() != null) {
             System.out.println(a.getContato2().getNome());
         }
@@ -43,6 +46,20 @@ public class Agenda {
         }
     }
 
+/* Exceção java.lang.ClassCastException
+ * estudando.exception.UncheckedExceptionMaisComuns.ContatoPF
+ * cannot be cast to 
+ * estudando.exception.UncheckedExceptionMaisComuns.ContatoPessoaJuridica
+ * 
+ * Significa que vc tentou fazer um cast para um tipo que não é suportado,
+ * Afinal você não pode fazer um cast de um ContatoPF para um tipo ContatoPJ
+ * isso ocorre pq o contato1 é uma instancia de ContatoPF
+ * Solução: antes de fazer o cast vc tem que verificar se vc pode fazer o cast
+ * e como você verifica? usando o operador instanceOf que verifica se uma
+ * determinada variavel/referencia que aponta pra um endereço na memoria é do tipo q eu
+ * quero testar, no caso: "se a var do tipo Contato PJ é do tipo ContatoPJ", se for:
+ * eu faço o cast, se não, fica como ContatoPF mesmo
+ */
     public void imprimirCNPJ(Agenda a) {
         
         if (a.getContato1() != null && a.getContato1() instanceof ContatoPJ) {
--- EstudandoJava/src/estudando/exception/UncheckedExceptionMaisComuns/Contato.java
+++ EstudandoJava/src/estudando/exception/UncheckedExceptionMaisComuns/Contato.java
@@ -1,10 +1,16 @@
 package estudando.exception.UncheckedExceptionMaisComuns;
 
-
+//Classe abstract só serve para ser herdada
 public abstract class Contato {
 
 	private String nome;
 	private Endereco endereco = new Endereco();
+	// 2º solução para não dar nullpointerexception
+	// private Endereco endereco = new Endereco();
+
+	/* public String getNome() { if(endereco == null){ endereco = new
+	 * Endereco(); } return nome; }
+	 */
 
 	public String getNome() {
 		if (endereco == null) {

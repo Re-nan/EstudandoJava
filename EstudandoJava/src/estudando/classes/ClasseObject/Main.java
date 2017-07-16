5f86e8471c0c657779fa06d876de2f369ac42f14
Renan <renan.ps@outlook.com>
2017-07-16 01:49:34 -0300

    Adicionando estudos sobre classe Object

---
+++ EstudandoJava/src/estudando/classes/ClasseObject/Main.java
@@ -0,0 +1,64 @@
+package estudando.classes.ClasseObject;
+
+/* Toda classe em Java herda de apenas uma classe, que também pode ser
+ * chamada de SUPERCLASSE. Se você não herdar sua classe de nenhuma
+ * outra, o java automaticamente faz ela herdar de Object
+ * 
+ * Object é a superclasse(classe mãe) de todas as classes do java
+ *   
+ * @author renan
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Object obj = new Object();
+		String nome = "RENAN";
+		Integer n1 = 10; 
+		
+		/* toString()
+		 * As classes podem sobrescrever este método para mostrarem
+		 * uma mensagem que as representem
+		 * o método System.out.prinltn(), por exemplo, usa este método
+		 */
+		System.out.println();
+		
+		
+		/* equals()
+		 * É a forma que o java tem de comparar ojetos pelo seu conteúdo
+		 * ao invés de comparar as referências(como acontece ao usarmos
+		 * "==")
+		 */
+		System.out.println();
+		
+		/* hashCode()
+		 * O java cria vários baldes, um que só aceita objs com hash, 5,
+		 * outro que só com 10 e outro que só aceita objs com hash 15
+		 * 
+		 * ao tirar o hash de um obj ele ve o numero e atribui ao balde
+		 * do numero sorteado, em um balde eu posso ter mais de um obj
+		 * com o mesmo hash
+		 * 
+		 * Eu posso ter vários objs com o mesmo hash, o que diferencia
+		 * é que ai entra o método equals encontra o elemento que você
+		 * está comparando dentro daquele balde
+		 * 
+		 * Resumindo: ele chama o hashCode() primeiro para identificar 
+		 * o local que ta e depois o equals
+		 * 
+		 * a regra é, se você for implementar o hashCode para criar sua 
+		 * prória lógica de hash, você deve implementar o equal também
+		 * e vice e versa, se implementou o equals na sua classe, então
+		 * implemente o hashCode também, essa é a boa prática, mas se
+		 * quiser pode usar o hashCode e equals como já são por padrão
+		 */
+		
+		
+		
+
+
+	}
+
+}

d342e58b9a476f3eeb8d87d591132e7e6c694e07
Renan <renan.ps@outlook.com>
2017-07-16 01:47:32 -0300

    Adicionando estudos sobre Polimorfismo

---
+++ EstudandoJava/src/estudando/Polimorfismo2/LeiteQuente.java
@@ -0,0 +1,23 @@
+package estudando.Polimorfismo2;
+
+public class LeiteQuente extends Bebida{
+    
+	/* Como ela herda de Bebida você tem que passar um construtor para ela
+	 * 
+	 * A classe bebida não possui um construtor implicito(aquele vazio),
+	 * então é necessário chmar o contrutor explicitamente usando o super
+	 * e passando seus devidos parametros
+	 */
+    public LeiteQuente(){
+        super("Leite Quente",true);
+    }
+
+    @Override
+    public void preparar() {
+        System.out.println("Pegando um copo");
+        System.out.println("Colando o leite no copo");
+    }
+    
+    
+}
+

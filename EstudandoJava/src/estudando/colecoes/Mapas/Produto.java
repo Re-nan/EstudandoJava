e6ebdca185a5a4c71602ca9a7125672965ded21c
Renan <renan.ps@outlook.com>
2017-07-19 00:37:13 -0300

    Adicionando estudos sobre Mapas

---
+++ EstudandoJava/src/estudando/colecoes/Mapas/Produto.java
@@ -0,0 +1,26 @@
+package estudando.colecoes.Mapas;
+
+public class Produto {
+	
+	private int id;
+	private String descricao;
+	
+	public Produto(int id, String descricao){
+		this.id = id;
+		this.descricao = descricao;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getDescricao(){
+		return descricao;
+	}
+	
+	@Override
+	public String toString(){
+		return id + ", " + descricao;
+	}
+
+}

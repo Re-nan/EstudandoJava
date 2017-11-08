35307940f1a2d1813be5f6f524ad6ec0df9af0a4
Renan <renan.ps@outlook.com>
2017-11-08 00:31:15 -0200

    Adicionando estudos sobre Abstract

---
+++ EstudandoJava/src/estudando/Abstract3/Conta.java
@@ -0,0 +1,22 @@
+package estudando.Abstract3;
+
+public abstract class Conta {
+
+protected double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo += valor;
+	}
+	
+	public void sacar(double valor) {
+		this.saldo -= valor;
+	}
+	
+	
+	public abstract void atualizar(double taxa);
+	
+}

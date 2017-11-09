383405b9c74697630f3d5864304bc7aa5daae2a6
Renan <renan.ps@outlook.com>
2017-11-09 00:34:34 -0200

    Adicionando estudos sobre Abstract

---
+++ EstudandoJava/src/estudando/Abstract3/ContaPoupanca.java
@@ -0,0 +1,14 @@
+package estudando.Abstract3;
+
+public class ContaPoupanca extends Conta {
+
+	@Override
+	public void atualizar(double taxa) {
+        this.saldo += this.saldo * taxa * 3;
+    }
+
+	@Override
+    public void depositar(double valor) {
+        this.saldo += valor - 0.10;
+    }
+}

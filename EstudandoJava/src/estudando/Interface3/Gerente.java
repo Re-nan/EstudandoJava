04cd1a70dc02f40ce6ba3e057489f85c4adfa798
Renan <renan.ps@outlook.com>
2017-11-12 01:57:47 -0200

    Adicionando estudos sobre Interface

---
+++ EstudandoJava/src/estudando/Interface3/Gerente.java
@@ -0,0 +1,26 @@
+package estudando.Interface3;
+
+public class Gerente extends Funcionario implements Autenticavel {
+
+	private String senha;
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	
+	@Override
+	public boolean autenticar(String senha) {
+		if(this.senha != senha) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+}

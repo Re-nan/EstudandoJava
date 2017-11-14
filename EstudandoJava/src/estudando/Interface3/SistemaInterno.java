ed8cb8ff8558716a1c4d26f409cd3da846153c0b
Renan <renan.ps@outlook.com>
2017-11-14 00:18:42 -0200

    Adicionando estudos sobre Interface

---
+++ EstudandoJava/src/estudando/Interface3/SistemaInterno.java
@@ -0,0 +1,26 @@
+package estudando.Interface3;
+
+public class SistemaInterno {
+
+	/* Autenticavel a = new Gerente();
+	 * posso aqui chamar o método autentica!
+	 * 
+	 *  Mas o mais adequado é passarmos uma referencia autenticavel como parametro
+	 */
+	
+	 boolean login(Autenticavel a) {
+	     
+		// pega senha de um lugar, ou de um scanner de polegar
+		String  senha = "123";
+				
+	     boolean ok = a.autenticar(senha);
+	     return ok;
+
+	     // aqui eu posso chamar o autentica! 
+	     // não necessariamente é um Funcionario!
+	     // Mais ainda, eu não sei que objeto a 
+	     // referência "a" está apontando exatamente! Flexibilidade.
+	  }
+	
+	 
+}

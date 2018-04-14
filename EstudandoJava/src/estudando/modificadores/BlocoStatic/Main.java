b3a54b02233fbb2776d6ca9f8b4f0d2cbeb755c4
Renan <renan.ps@outlook.com>
2018-04-14 02:28:13 -0300

    Adicionando estudos sobre Bloco Static

---
+++ EstudandoJava/src/estudando/modificadores/BlocoStatic/Main.java
@@ -0,0 +1,15 @@
+package estudando.modificadores.BlocoStatic;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa();
+        
+        Pessoa p2 = new Pessoa();
+        
+        Pessoa p3 = new Pessoa();
+        
+        /* Perceba que só exibiu o bloco static 1 vez */
+    }
+}
+

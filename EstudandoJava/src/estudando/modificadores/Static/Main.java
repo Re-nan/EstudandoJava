940a5fa6c5959ea9b7b94444abcd588fb5978b61
Renan <renan.ps@outlook.com>
2018-04-18 22:58:39 -0300

    Adicionando estudos sobre Static

---
+++ EstudandoJava/src/estudando/modificadores/Static/Main.java
@@ -0,0 +1,13 @@
+package estudando.modificadores.Static;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ContaBancaria conta = new ContaBancaria();
+		
+		System.out.println(ContaBancaria.getBanco());
+
+	}
+
+}

fbd810c1b4440f1b3cd01cb4e4503f610c1fb242
Renan <renan.ps@outlook.com>
2018-05-09 15:03:51 -0300

    Adicionando estudos sobre Herança

--- EstudandoJava/src/estudando/poo/Heranca/Main.java
+++ EstudandoJava/src/estudando/poo/Heranca/Main.java
@@ -1,5 +1,27 @@
-package estudando.Heranca;
+package estudando.poo.Heranca;
 
+/* HERANÇA é um mecanismo que permite que uma classe possa herdar o comportamento de outra
+ * classe, ao mesmo tempo em que novos comportamentos podem ser estabelecidos. A ideia é agrupar
+ * coisas comuns para poder reaproveitar código
+ * 
+ * A SUB CLASSE herda o comportamento da SUPER CLASSE
+ * Herenaça é uma relação que chamamos de "É UM" (Ex. Cachorro e Gato É UM Animal logo Chacorro
+ * e Gato herdam de Animal)
+ * 
+ * Toda classe java pode herdar somente uma classe, independente de você dedefinir ou não uma 
+ * classe para herdar o Java herda automaticamento de Object, direta ou indiretamente
+ * 
+ * OVERRIDING quando uma classe herda d eoutra ela pode sobrescrever/redefinir os métodos da 
+ * classe que ela herdou, VANTAGEM: na super classe pode ter um código genérico, e na sub classe
+ * pode sobreescrever para um códgigo específico
+ * 
+ * Se a classe mãe mãe tem o metodo telefonar() e a classe filha tem o método telefonar(int nr)
+ * então não há sobreescrita, para ser considerado sobreescrita os métodos precisam terem a mesma
+ * assinatura(tipo de retorno, nome do metodo, mesma qtd e tipo dos parametros)
+ * 
+ * SUPER o método que foi sobreescrito/redefinido pode acessar o método original através da palavra
+ * super, Ex dentro do método que deu override usar super.NomeDoMetodo 
+ */
 public class Main {
     public static void main(String[] args){
     	

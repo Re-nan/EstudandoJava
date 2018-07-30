c7285315161b10988aeb9a9aeb65fd49968d4619
Renan <renan.ps@outlook.com>
2018-07-30 10:34:17 -0300

    Adicionando estudos sobre classe Object

---
+++ EstudandoJava/src/estudando/classes/ClasseObject/Pessoa.java
@@ -0,0 +1,15 @@
+package estudando.classes.ClasseObject;
+
+public class Pessoa {
+	
+	String nome;
+
+	/*@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + "]";
+	}*/
+	
+
+	
+
+}

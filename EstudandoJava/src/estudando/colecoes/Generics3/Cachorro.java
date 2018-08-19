c8f0dd17fe2aacd4642ba66e8cd10d905e7d0273
Renan <renan.ps@outlook.com>
2018-08-19 00:08:27 -0300

    Adicionando estudos sobre Generics

---
+++ EstudandoJava/src/estudando/colecoes/Generics3/Cachorro.java
@@ -0,0 +1,14 @@
+package estudando.colecoes.Generics3;
+
+public class Cachorro extends Animal {
+
+	Cachorro(String nome){
+		this.nome = nome;
+	}
+
+	public String latir() {
+		return "Au Au!";
+	}
+	
+	
+}

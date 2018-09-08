1dc78a130377ab4994e168caf612325a2b71b198
Renan <renan.ps@outlook.com>
2018-09-07 23:20:34 -0300

    Adicionando estudos sobre Annotations

---
+++ EstudandoJava/src/estudando/Annotations/Main.java
@@ -0,0 +1,18 @@
+package estudando.Annotations;
+
+/* Annotations são um mecanismo criado a partir do Java 5
+ * - São declarações no código que podem ser usado por ferramentas ou programas externos
+ * - Não influenciam diretamente na execução do código
+ * - Uma annotations anota o elemento que vem logo abaixo dela: se eu usar acima da definição
+ * de uma classe então eu estou anotando a classe. Mas também ppodem serem usadas para anotar
+ * métodos, construtores, atributos e etc...
+ */
+
+@ClassInfo(autor = "renan",  data = "20/08/2018")
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}

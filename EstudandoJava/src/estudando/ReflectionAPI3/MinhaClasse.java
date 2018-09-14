cf5663e0c031181bc9a074f360f0baa3cccf9c9c
Renan <renan.ps@outlook.com>
2018-09-14 05:52:48 -0300

    Adicionando estudos sobre Reflection API

---
+++ EstudandoJava/src/estudando/ReflectionAPI3/MinhaClasse.java
@@ -0,0 +1,18 @@
+package estudando.ReflectionAPI3;
+
+public class MinhaClasse {
+
+	@Executar
+	public void m1() {
+		System.out.println("m1");
+	}
+	
+	public void m2() {
+		System.out.println("m2");
+	}
+	
+	public void m3() {
+		System.out.println("m3");
+	}
+	
+}

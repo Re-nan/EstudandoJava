5f25c7f814137bfbe9d4dc952546ca30a90a9056
Renan <renan.ps@outlook.com>
2018-10-02 01:12:37 -0300

    Adicionando estudos sobre Anonymous Inner Class

---
+++ EstudandoJava/src/estudando/innerclass/AnonymousInnerClass/Cafe.java
@@ -0,0 +1,10 @@
+package estudando.innerclass.AnonymousInnerClass;
+
+public class Cafe implements Bebida {
+
+	@Override
+	public void preparar() {
+		System.out.println("Preparando Café");
+	}
+	
+}

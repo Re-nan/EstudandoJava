448872e7b78b5e33a0fa1e37ea98cc2e87b42e2f
Re-nan <renan.ps@outlook.com>
2019-04-14 00:25:29 -0300

    Adicionando estudos sobre Construtores

---
+++ EstudandoJava/src/estudando/poo/Construtores2/Funcionario.java
@@ -0,0 +1,23 @@
+package estudando.poo.Construtores2;
+
+public class Funcionario {
+
+	//	private String nome;
+	
+	
+	public Funcionario() {
+		System.out.println("Criando Funcionário");
+	}
+	
+
+	
+	/* Forçaria todos os construtores das classes
+	 * que herdam Funcionario a implementarem o
+	 * parametro também
+	 */
+	
+	//public Funcionario(String nome) {
+	//	System.out.println("Criando Funcionário");
+	//}
+
+}
